42f1b1b7333fd5fbc8100d0f188304429fe3198d
mojpm <itsjoelpm@gmail.com>
2021-01-06 20:39:25 +0300

    minor update

--- src/components/menuButton.js
+++ src/components/menuButton.js
@@ -1,18 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi';
-import Menu from './menu'
-
-
+import Menu from './Menu'
 
 const MenuButton = () => {
 
-    const toggleMenu = () => {
-        console.log('Working');
-        <Menu />
-    }
-    return (
+    const [showMenu, setShowMenu] = useState(false)
 
-        <button className="menu" onClick={toggleMenu} >
+    const menuShow = {
+        showMenu: {
+            color: 'blue',
+            position: 'absolute',
+            top: '70px',
+            right: '80px',
+
+            width: '200px',
+            height: '250px',
+            background: 'rgb(190, 179, 179)',
+
+            borderRadius: '8px',
+            opacity: '1',
+
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-evenly',
+            alignItems: 'center'
+        },
+
+        menuButtons: {
+            border: 'none',
+            outline: 'none',
+            background: 'none',
+            width: '80%',
+            padding: '10px',
+        },
+
+        searchButton: {
+            fontFamily: 'Arial',
+        },
+
+        icon: {
+            position: 'relative',
+            left: '-5px',
+            top: '2px',
+        }
+    }
+
+    const displayMenu = () => {
+        console.log('Working');
+        if (!showMenu) {
+            <Menu />
+            setShowMenu(true)
+        }
+    }
+
+    return (
+        <button className="menu" onClick={displayMenu} >
             <GiHamburgerMenu size="28" />
         </button >
     )
