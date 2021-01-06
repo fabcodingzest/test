76d0c2d24f5484a427d60166ff7a00b260b5c85f
mojpm <itsjoelpm@gmail.com>
2021-01-06 20:39:05 +0300

    minor update

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import './menu.css';
+import './Menu.css';
+import { BsSearch, BsHeartFill } from 'react-icons/bs';
+import { CgNotes, CgMenuGridR } from 'react-icons/cg';
+import { FaPaintBrush } from 'react-icons/fa';
 
 const Menu = () => {
+  const number = 12;
   return (
-    <div className="main">
-      <h4>Search</h4>
-      <h4>Add Own Quote</h4>
-      <h4>Favorites</h4>
-      <h4>Theme</h4>
+    <div className="show-menu">
+      <button className="menu-buttons search-button">
+        <BsSearch className="icon" /> Search
+      </button>
+      <button className="menu-buttons">
+        <CgNotes className="icon" /> Add Your Own
+      </button>
+      <button className="menu-buttons">
+        <BsHeartFill className="icon" /> Favorites({number}){' '}
+      </button>
+      <button className="menu-buttons">
+        <CgMenuGridR className="icon" /> Categories
+      </button>
+      <button className="menu-buttons">
+        <FaPaintBrush className="icon" /> Theme
+      </button>
     </div>
   );
 };
