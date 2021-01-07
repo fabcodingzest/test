cb5168f982c591c8d8f5ff8a4e2f2e944531ff00
mojpm <itsjoelpm@gmail.com>
2021-01-07 19:31:24 +0300

    Updare App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
-import quotes from './data/quotes';
-import MenuButton from './components/menuButton';
-import ModeButton from './components/modeButton';
-import Quote from './components/quote';
+import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import Theme from './pages/Theme';
+import OwnQuote from './pages/OwnQuote';
+import { BsSun } from 'react-icons/bs';
 
 const App = () => {
+
+  const ModeButton = () => (
+    <button className="mode">
+      <BsSun size="28" />
+    </button>
+  )
+
   return (
     <>
+
       <ModeButton />
-      <MenuButton />
-      <Quote quotes={quotes} />
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/search' component={Search} />
+        <Route path='/search' component={Favorites} />
+        <Route path='/search' component={Theme} />
+        <Route path='/search' component={OwnQuote} />
+      </Switch>
     </>
   );
 };
