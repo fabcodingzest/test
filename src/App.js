c4fe0270d8f1a605b30211d98f879e8501745e6a
mojpm <itsjoelpm@gmail.com>
2021-01-08 14:21:41 +0300

    minor updates

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
-import './App.css';
+import SearchButton from './components/searchButton';
+import * as BsIcons from 'react-icons/bs';
 import Navbar from './components/Navbar'
 import Home from './pages/Home';
-import Search from './pages/Search';
 import Favorites from './pages/Favorites';
 import Theme from './pages/Theme';
 import OwnQuote from './pages/OwnQuote';
-import { BsSun } from 'react-icons/bs';
+import './App.css';
 
 const App = () => {
 
   const ModeButton = () => (
     <button className="mode">
-      <BsSun size="28" />
+      <BsIcons.BsSun size="28" />
     </button>
   )
 
   return (
     <>
-
       <ModeButton />
+      <SearchButton />
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
-        <Route path='/search' component={Search} />
-        <Route path='/search' component={Favorites} />
-        <Route path='/search' component={Theme} />
-        <Route path='/search' component={OwnQuote} />
+        <Route path='/favorites' component={Favorites} />
+        <Route path='/theme' component={Theme} />
+        <Route path='/own-quote' component={OwnQuote} />
       </Switch>
     </>
   );
