74d474d1ccdbbf75659aa4d2e37f3639ae35e08f
mojpm <itsjoelpm@gmail.com>
2021-01-11 10:17:03 +0300

    initial commit v2.0

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
-import SearchButton from './components/searchButton';
-import * as BsIcons from 'react-icons/bs';
-import Navbar from './components/Navbar'
-import Home from './pages/Home';
-import Favorites from './pages/Favorites';
-import Theme from './pages/Theme';
-import OwnQuote from './pages/OwnQuote';
 import './App.css';
 
 const App = () => {
-
-  const ModeButton = () => (
-    <button className="mode">
-      <BsIcons.BsSun size="28" />
-    </button>
-  )
-
-  return (
-    <>
-      <ModeButton />
-      <SearchButton />
-      <Navbar />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/favorites' component={Favorites} />
-        <Route path='/theme' component={Theme} />
-        <Route path='/own-quote' component={OwnQuote} />
-      </Switch>
-    </>
-  );
+  return <h1>Hello Motivate-me</h1>;
 };
 
 export default App;
